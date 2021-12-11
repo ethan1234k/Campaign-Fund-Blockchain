410e6a677a6f23e7284f80d9ac9d7c3357a035de
ethan1234k <ethanwoodhill@gmail.com>
2021-12-10 20:41:52 -0800

    Added mocha tests to campaign.test.js

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -29,5 +29,26 @@ beforeEach(async() => {
     campaign = await new web3.eth.Contract(
         JSON.parse(compiledCampaign.interface),
         campaignAddress
-    )
+    );
 });
+
+describe('Campaigns', () => {
+    it ("Factory and Campaign Deployed", () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
+
+    it ("Correctly Marks Manager", async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.strictEqual(manager, accounts[0]);
+    });
+
+    it ("Allows Contributions, Marks Donor as Contributor", async () => {
+        await campaign.methods.contribute().send({
+            value: '300',
+            from: accounts[1]
+        })
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
+})
